a8ee93ceaba1ce7bda3f2a48491c271f348200d6
lxhcaicai <2778763221@qq.com>
2023-09-07 23:59:41 +0800

    Redission 二进制流存储

--- src/test/java/com/github/redission/RedissionTester.java
+++ src/test/java/com/github/redission/RedissionTester.java
@@ -2,10 +2,13 @@ package com.github.redission;
 
 import org.junit.jupiter.api.Test;
 import org.redisson.Redisson;
+import org.redisson.api.RBinaryStream;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
 public class RedissionTester {
@@ -31,5 +34,33 @@ public class RedissionTester {
 
         // 关闭客户端
         redissonClient.shutdown();
+    }
+
+    @Test
+    public void RedissonStream() throws IOException {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://127.0.0.1:6379")
+                .setPassword("123456");
+        RedissonClient redissonClient = Redisson.create(config);
+
+        //====================操作流来存储对象====================
+        RBinaryStream stream = redissonClient.getBinaryStream("stream");
+        stream.set("name is".getBytes());
+        OutputStream outputStream = stream.getOutputStream();
+
+        outputStream.write("victory".getBytes());
+        InputStream inputStream = stream.getInputStream();
+
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = inputStream.read(bytes)) != -1) {
+            result.write(bytes, 0, length);
+        }
+        System.out.println(result.toString());
+        // 关闭客户端
+        redissonClient.shutdown();
+
     }
 }
